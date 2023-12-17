118899c8e89733b99e233ed16889644464f81304
mr.1g0r <mr.1g0r@icloud.com>
2023-12-17 23:01:05 +0000

    Minor re-factoring and cosmetic changes

--- src/main/java/four/Board.java
+++ src/main/java/four/Board.java
@@ -24,15 +24,37 @@ public class Board extends JPanel {
 
     private Player currentMove;
     private final int[] nextEmptyCellPerColumn;
+    private final WinningCellsDetector winningCellsDetector;
 
     public Board() {
         this.currentMove = playerOne;
 
         this.cells = new JButton[MAX_COLUMNS][MAX_ROWS];
         this.nextEmptyCellPerColumn = new int[MAX_COLUMNS];
-        setLayout(new GridLayout(MAX_ROWS, MAX_COLUMNS));
+        this.winningCellsDetector = new WinningCellsDetector(this.cells);
 
+        setLayout(new GridLayout(MAX_ROWS, MAX_COLUMNS));
         createBoardButtons();
+    }
+
+    public void render() {
+        for (int y = cells[0].length - 1; y >= 0; y--) {
+            for (JButton[] cell : cells) {
+                add(cell[y]);
+            }
+        }
+    }
+
+    public void reset() {
+        for (int x = 0; x < Board.MAX_COLUMNS; x++) {
+            for (int y = Board.MAX_ROWS - 1; y >= 0; y--) {
+                JButton button = cells[x][y];
+                button.setText(EMPTY_CELL_TEXT);
+                button.setBackground(BUTTON_BACKGROUND_COLOUR);
+            }
+            nextEmptyCellPerColumn[x] = 0;
+        }
+        currentMove = playerOne;
     }
 
     private void createBoardButtons() {
@@ -54,7 +76,7 @@ public class Board extends JPanel {
         button.setBackground(BUTTON_BACKGROUND_COLOUR);
         button.setPreferredSize(CELL_DRAWING_SIZE);
 
-        // add white border and removes "..." as initial basic button text
+        // adds white border and removes "..." as initial basic button text
         button.setBorder(new MatteBorder(1, 1, 1, 1, Color.WHITE));
         button.setMargin(new Insets(0, 0, 0, 0));    // removes margins
 
@@ -71,7 +93,7 @@ public class Board extends JPanel {
 
         if (isMoveValid(x, y)) {
             cells[x][y].setText(currentMove.piece());
-            List<JButton> winningCells = getWinningCells(x, y);
+            List<JButton> winningCells = winningCellsDetector.getWinningCells(x, y);
             if (winningCells.size() == 4) {
                 winningCells.forEach(cell -> cell.setBackground(BUTTON_VICTORY_COLOUR));
                 currentMove = null; // Game Ended
@@ -81,88 +103,6 @@ public class Board extends JPanel {
         } else {
             Toolkit.getDefaultToolkit().beep();
         }
-    }
-
-    private List<JButton> getWinningCells(final int x, final int y) {
-        var winningCells = new ArrayList<JButton>();
-        winningCells.addAll(getHorizontalWinningCells(y));
-        winningCells.addAll(getVerticalWinningCells(x));
-        winningCells.addAll(getDiagonalWinningCells());
-        return winningCells;
-    }
-
-    private List<JButton> getDiagonalWinningCells() {
-        String currentPlayer = currentMove.piece();
-        List<JButton> winningCells = new ArrayList<>();
-
-        for (int i = 0; i < MAX_COLUMNS - 3; i++) {
-            for (int j = 0; j < MAX_ROWS - 3; j++) {
-                var firstCell = cells[i][j];
-                var secondCell = cells[i+1][j+1];
-                var thirdCell = cells[i+2][j+2];
-                var forthCell = cells[i+3][j+3];
-
-                if (currentPlayer.equals(firstCell.getText())
-                        && firstCell.getText().equals(secondCell.getText())
-                        && secondCell.getText().equals(thirdCell.getText())
-                        && thirdCell.getText().equals(forthCell.getText())) {
-                    winningCells.addAll(List.of(firstCell, secondCell, thirdCell, forthCell));
-                }
-            }
-        }
-
-        for (int i = 0; i < MAX_COLUMNS - 3; i++) {
-            for (int j = 3; j < MAX_ROWS; j++) {
-                var firstCell = cells[i][j];
-                var secondCell = cells[i+1][j-1];
-                var thirdCell = cells[i+2][j-2];
-                var forthCell = cells[i+3][j-3];
-
-                if (currentPlayer.equals(firstCell.getText())
-                        && firstCell.getText().equals(secondCell.getText())
-                        && secondCell.getText().equals(thirdCell.getText())
-                        && thirdCell.getText().equals(forthCell.getText())) {
-                    winningCells.addAll(List.of(firstCell, secondCell, thirdCell, forthCell));
-                }
-            }
-        }
-        return winningCells;
-    }
-
-    private List<JButton> getVerticalWinningCells(final int x) {
-        String currentPlayer = currentMove.piece();
-        for (int row = 0; row < MAX_ROWS - 3; row++) {
-            var firstCell = cells[x][row];
-            var secondCell = cells[x][row+1];
-            var thirdCell = cells[x][row+2];
-            var forthCell = cells[x][row+3];
-
-            if (currentPlayer.equals(firstCell.getText())
-                    && firstCell.getText().equals(secondCell.getText())
-                    && secondCell.getText().equals(thirdCell.getText())
-                    && thirdCell.getText().equals(forthCell.getText())) {
-                return List.of(firstCell, secondCell, thirdCell, forthCell);
-            }
-        }
-        return Collections.emptyList();
-    }
-
-    private List<JButton> getHorizontalWinningCells(final int y) {
-        String currentPlayer = currentMove.piece();
-        for (int column = 0; column < MAX_COLUMNS - 3; column++) {
-            var firstCell = cells[column][y];
-            var secondCell = cells[column+1][y];
-            var thirdCell = cells[column+2][y];
-            var forthCell = cells[column+3][y];
-
-            if (currentPlayer.equals(firstCell.getText())
-                && firstCell.getText().equals(secondCell.getText())
-                && secondCell.getText().equals(thirdCell.getText())
-                && thirdCell.getText().equals(forthCell.getText())) {
-                return List.of(firstCell, secondCell, thirdCell, forthCell);
-            }
-        }
-        return Collections.emptyList();
     }
 
     private boolean isMoveValid(final int x, final int y) {
@@ -176,23 +116,94 @@ public class Board extends JPanel {
         currentMove = currentMove == playerOne ? playerTwo : playerOne;
     }
 
-    public void render() {
-        for (int y = cells[0].length - 1; y >= 0; y--) {
-            for (JButton[] cell : cells) {
-                add(cell[y]);
-            }
-        }
-    }
 
-    public void reset() {
-        for (int x = 0; x < Board.MAX_COLUMNS; x++) {
-            for (int y = Board.MAX_ROWS - 1; y >= 0; y--) {
-                JButton button = cells[x][y];
-                button.setText(EMPTY_CELL_TEXT);
-                button.setBackground(BUTTON_BACKGROUND_COLOUR);
-            }
-            nextEmptyCellPerColumn[x] = 0;
+    private class WinningCellsDetector {
+        private final JButton[][] cells;
+
+        WinningCellsDetector(JButton[][] cells) {
+            this.cells = cells;
         }
-        currentMove = playerOne;
+
+        List<JButton> getWinningCells(final int x, final int y) {
+            var winningCells = new ArrayList<JButton>();
+            winningCells.addAll(getHorizontalWinningCells(y));
+            winningCells.addAll(getVerticalWinningCells(x));
+            winningCells.addAll(getDiagonalWinningCells());
+            return winningCells;
+        }
+
+        private List<JButton> getDiagonalWinningCells() {
+            String currentPlayer = currentMove.piece();
+            List<JButton> winningCells = new ArrayList<>();
+
+            for (int i = 0; i < MAX_COLUMNS - 3; i++) {
+                for (int j = 0; j < MAX_ROWS - 3; j++) {
+                    var firstCell = cells[i][j];
+                    var secondCell = cells[i + 1][j + 1];
+                    var thirdCell = cells[i + 2][j + 2];
+                    var forthCell = cells[i + 3][j + 3];
+
+                    if (currentPlayer.equals(firstCell.getText())
+                            && firstCell.getText().equals(secondCell.getText())
+                            && secondCell.getText().equals(thirdCell.getText())
+                            && thirdCell.getText().equals(forthCell.getText())) {
+                        winningCells.addAll(List.of(firstCell, secondCell, thirdCell, forthCell));
+                    }
+                }
+            }
+
+            for (int i = 0; i < MAX_COLUMNS - 3; i++) {
+                for (int j = 3; j < MAX_ROWS; j++) {
+                    var firstCell = cells[i][j];
+                    var secondCell = cells[i + 1][j - 1];
+                    var thirdCell = cells[i + 2][j - 2];
+                    var forthCell = cells[i + 3][j - 3];
+
+                    if (currentPlayer.equals(firstCell.getText())
+                            && firstCell.getText().equals(secondCell.getText())
+                            && secondCell.getText().equals(thirdCell.getText())
+                            && thirdCell.getText().equals(forthCell.getText())) {
+                        winningCells.addAll(List.of(firstCell, secondCell, thirdCell, forthCell));
+                    }
+                }
+            }
+            return winningCells;
+        }
+
+        private List<JButton> getVerticalWinningCells(final int x) {
+            String currentPlayer = currentMove.piece();
+            for (int row = 0; row < MAX_ROWS - 3; row++) {
+                var firstCell = cells[x][row];
+                var secondCell = cells[x][row + 1];
+                var thirdCell = cells[x][row + 2];
+                var forthCell = cells[x][row + 3];
+
+                if (currentPlayer.equals(firstCell.getText())
+                        && firstCell.getText().equals(secondCell.getText())
+                        && secondCell.getText().equals(thirdCell.getText())
+                        && thirdCell.getText().equals(forthCell.getText())) {
+                    return List.of(firstCell, secondCell, thirdCell, forthCell);
+                }
+            }
+            return Collections.emptyList();
+        }
+
+        private List<JButton> getHorizontalWinningCells(final int y) {
+            String currentPlayer = currentMove.piece();
+            for (int column = 0; column < MAX_COLUMNS - 3; column++) {
+                var firstCell = cells[column][y];
+                var secondCell = cells[column + 1][y];
+                var thirdCell = cells[column + 2][y];
+                var forthCell = cells[column + 3][y];
+
+                if (currentPlayer.equals(firstCell.getText())
+                        && firstCell.getText().equals(secondCell.getText())
+                        && secondCell.getText().equals(thirdCell.getText())
+                        && thirdCell.getText().equals(forthCell.getText())) {
+                    return List.of(firstCell, secondCell, thirdCell, forthCell);
+                }
+            }
+            return Collections.emptyList();
+        }
     }
 }
